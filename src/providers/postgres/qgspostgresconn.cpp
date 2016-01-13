032931a7b779144f813ce0dcff1bca28df492ff7
Luigi Pirelli <luipir@gmail.com>
2016-01-13 13:28:10 +0100

    remove certs just after connection

--- src/providers/postgres/qgspostgresconn.cpp
+++ src/providers/postgres/qgspostgresconn.cpp
@@ -207,6 +207,21 @@ QgsPostgresConn::QgsPostgresConn( const QString& conninfo, bool readOnly, bool s
   QString expandedConnectionInfo = uri.connectionInfo(true);
 
   mConn = PQconnectdb( expandedConnectionInfo.toLocal8Bit() );  // use what is set based on locale; after connecting, use Utf8
+
+  // remove temporary cert/key/CA
+  QgsDataSourceURI expandedUri(expandedConnectionInfo);
+  QString sslCertFile = expandedUri.param("sslcert");
+  sslCertFile.remove("'");
+  QFile::remove(sslCertFile);
+
+  QString sslKeyFile = expandedUri.param("sslkey");
+  sslKeyFile.remove("'");
+  QFile::remove(sslKeyFile);
+
+  QString sslCAFile = expandedUri.param("sslrootcert");
+  sslCAFile.remove("'");
+  QFile::remove(sslCAFile);
+
   // check the connection status
   if ( PQstatus() != CONNECTION_OK )
   {
