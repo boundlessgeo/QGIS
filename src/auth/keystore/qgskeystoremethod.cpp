0ba0668a919f2153c896c9b0b4630cc1b274bd97
Luigi Pirelli <luipir@gmail.com>
2016-09-05 15:21:06 +0200

    revert conditional build because jenkins fails

--- src/auth/keystore/qgskeystoremethod.cpp
+++ src/auth/keystore/qgskeystoremethod.cpp
@@ -14,7 +14,7 @@
  *                                                                         *
  ***************************************************************************/
 
-#ifdef Q_OS_WIN
+//#ifdef Q_OS_WIN
 
 #include "qgskeystoremethod.h"
 #include "qgskeystoreedit.h"
@@ -349,4 +349,4 @@ QGISEXTERN void cleanupAuthMethod() // pass QgsAuthMethod *method, then delete m
 {
 }
 
-#endif // Q_OS_WIN
+//#endif // Q_OS_WIN
